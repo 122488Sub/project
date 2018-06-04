e1bb109490f9e65c28115bbe4a8bebe3e2ebb21e
122488Sub <yello0105@gmail.com>
2018-06-04 19:39:53 +0900

    메뉴버튼생성

--- workspace/Pos/src/MainPanel.java
+++ workspace/Pos/src/MainPanel.java
@@ -11,43 +11,44 @@ import java.awt.event.*;
 //**********************************************************************
 
 public class MainPanel extends JPanel {
-	
-	//**********************************************************************
-	//***변수***
-	//**********************************************************************
-	//상수 & DB값
+
+	// **********************************************************************
+	// ***변수***
+	// **********************************************************************
+	// 상수 & DB값
 	DataInfo data;
 	DbDAO DB;
-	//전체 좌석화면
+	// 전체 좌석화면
 	private SeatAllPanel main_Panel;
-	//선택 좌석화면
+	// 선택 좌석화면
 	private SeatPanel seat_Panel;
-	
-	//**********************************************************************
-	//***MainPanel 생성자 - 모든 클래스 생성 및 이벤트 관리***
-	//**********************************************************************
+
+	// **********************************************************************
+	// ***MainPanel 생성자 - 모든 클래스 생성 및 이벤트 관리***
+	// **********************************************************************
 	public MainPanel() throws SQLException {
 
-		//기본크기 800x600
+		// 기본크기 800x600
 		setPreferredSize(new Dimension(800, 600));
 		setBackground(Color.WHITE);
 		setLayout(null);
 		DB = new DbDAO();
 		data = new DataInfo();
-		//**********************************************************************
+		// **********************************************************************
 		//
 		// 전체 좌석화면 설정
 		//
-		main_Panel= new SeatAllPanel();
-		
+		main_Panel = new SeatAllPanel();
+
 		////
-		////전체 좌석화면: 좌석 선택시 화면전환 이벤트 실시
+		//// 전체 좌석화면: 좌석 선택시 화면전환 이벤트 실시
 		////
 		main_Panel.seat1_Panel.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				seat_AlltoSel_Change(1);
 			}
+
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				seat_AlltoSel_Change(1);
@@ -58,6 +59,7 @@ public class MainPanel extends JPanel {
 			public void mouseClicked(MouseEvent e) {
 				seat_AlltoSel_Change(2);
 			}
+
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				seat_AlltoSel_Change(2);
@@ -68,6 +70,7 @@ public class MainPanel extends JPanel {
 			public void mouseClicked(MouseEvent e) {
 				seat_AlltoSel_Change(3);
 			}
+
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				seat_AlltoSel_Change(3);
@@ -78,6 +81,7 @@ public class MainPanel extends JPanel {
 			public void mouseClicked(MouseEvent e) {
 				seat_AlltoSel_Change(4);
 			}
+
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				seat_AlltoSel_Change(4);
@@ -88,6 +92,7 @@ public class MainPanel extends JPanel {
 			public void mouseClicked(MouseEvent arg0) {
 				seat_AlltoSel_Change(5);
 			}
+
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				seat_AlltoSel_Change(5);
@@ -98,6 +103,7 @@ public class MainPanel extends JPanel {
 			public void mouseClicked(MouseEvent e) {
 				seat_AlltoSel_Change(6);
 			}
+
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				seat_AlltoSel_Change(6);
@@ -108,6 +114,7 @@ public class MainPanel extends JPanel {
 			public void mouseClicked(MouseEvent e) {
 				seat_AlltoSel_Change(7);
 			}
+
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				seat_AlltoSel_Change(7);
@@ -118,51 +125,55 @@ public class MainPanel extends JPanel {
 			public void mouseClicked(MouseEvent e) {
 				seat_AlltoSel_Change(8);
 			}
+
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				seat_AlltoSel_Change(8);
 			}
 		});
-		//**********************************************************************
+		// **********************************************************************
 		//
 		// 좌석 선택시 화면
 		seat_Panel = new SeatPanel();
 		//
 		////
-		////좌석선택화면: 취소버튼
+		//// 좌석선택화면: 취소버튼
 		////
 		seat_Panel.select_Cancel_Button.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				seat_SeltoAll_Cancel_Change();
 			}
+
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				seat_SeltoAll_Cancel_Change();
 			}
 		});
 		////
-		////좌석선택화면: 확인버튼
+		//// 좌석선택화면: 확인버튼
 		////
 		seat_Panel.select_Confirm_Button.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				seat_SeltoAll_Confirm_Change();
 			}
+
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				seat_SeltoAll_Confirm_Change();
 			}
 		});
 		////
-		////좌석선택화면: 결제버튼
+		//// 좌석선택화면: 결제버튼
 		////
 		seat_Panel.select_Pay_Button.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				
+
 				seat_SeltoAll_Pay_Change();
 			}
+
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				seat_SeltoAll_Pay_Change();
@@ -170,71 +181,65 @@ public class MainPanel extends JPanel {
 			//
 		});
 		
-		
-		//**********************************************************************
-		//처음에는 전체좌석화면만 보이도록 설정
+		// **********************************************************************
+		// 처음에는 전체좌석화면만 보이도록 설정
 		this.add(main_Panel.seatAll_Panel);
 		this.add(seat_Panel.seatSelect_Panel);
 	}
-	
-	//**********************************************************************
-	//***화면 전환 관리 함수***
-	//**********************************************************************
+
+	// **********************************************************************
+	// ***화면 전환 관리 함수***
+	// **********************************************************************
 
 	public void seat_SeltoAll_Cancel_Change() {
-		//취소(주문완료 안하고 취소) DB저장안하고 그냥나오기
+		// 취소(주문완료 안하고 취소) DB저장안하고 그냥나오기
 		main_Panel.seatAll_Panel.setVisible(true);
 		seat_Panel.seatSelect_Panel.setVisible(false);
 	}
-	
+
 	public void seat_SeltoAll_Confirm_Change() {
-		//확인(메뉴주문)시 DB저장 필요
+		// 확인(메뉴주문)시 DB저장 필요
 		main_Panel.seatAll_Panel.setVisible(true);
 		seat_Panel.seatSelect_Panel.setVisible(false);
 	}
-	
+
 	public void seat_SeltoAll_Pay_Change() {
-		//결제와 관련된 창 필요
-		//DB추가이후 작업
-		try{
+		// 결제와 관련된 창 필요
+		// DB추가이후 작업
+		try {
 			/*
 			 * 결제버튼 클릭시 data 삽입 삭제
 			 */
 			data.insertSales(DB.getSeatNum());
-		}catch(SQLException S)
-		{
-			
+		} catch (SQLException S) {
+
 		}
 	}
-	
+
 	public void seat_AlltoSel_Change(int _seatNum) {
 		DB.setSeatNum(_seatNum);
 		ArrayList<DbDAO> li = new ArrayList<DbDAO>();
 		try {
 			li = data.GetSeat(DB.getSeatNum());
-			for(int i=0;i<li.size();i++) {
+			for (int i = 0; i < li.size(); i++) {
 				System.out.println(i);
 				DbDAO order = li.get(i);
 				System.out.println(order.getMenuNum());
 				System.out.println(order.getName());
 				System.out.println(order.getNumber());
 				System.out.println(order.getTotaprice());
-				
-				
-				
+
 			}
-		}catch(SQLException e) {
-			
+		} catch (SQLException e) {
+
 		}
-		//변경시 각 좌석별 내용 변동시키기
-		seat_Panel.seatNum_Label.setText("No."+DB.getSeatNum() + " Table");
-		//***선택 좌석의 현재 주문상태 추가하기***
-		
-						
-		
-		//패널전환
+		// 변경시 각 좌석별 내용 변동시키기
+		seat_Panel.seatNum_Label.setText("No." + DB.getSeatNum() + " Table");
+		// ***선택 좌석의 현재 주문상태 추가하기***
+
+		// 패널전환
 		main_Panel.seatAll_Panel.setVisible(false);
 		seat_Panel.seatSelect_Panel.setVisible(true);
-		
+		seat_Panel.Change_chic();
 	}
 }
--- workspace/Pos/src/SeatPanel.java
+++ workspace/Pos/src/SeatPanel.java
@@ -1,71 +1,290 @@
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class SeatPanel extends JPanel {
-	
-	public JPanel seatSelect_Panel;
-	
-	public JPanel Order_Panel, MenuPanel;
-	public JButton select_Confirm_Button, select_Pay_Button, select_Cancel_Button;
+
 	public JLabel seatNum_Label;
+
+	public JPanel seatSelect_Panel;
+
+	public JPanel Order_Panel;
+
+	public JPanel MenuPanel;
+	public JPanel menuPan_Panel;
+	public JLabel chic_Label, drink_Label, side_Label;
+
+	public JButton select_Confirm_Button, select_Pay_Button, select_Cancel_Button;
+
+	DataInfo data;
+	DbDAO DB;
+	private ArrayList<DbDAO> cickArray, sideArray, drinkArray;
 	
-	public SeatPanel() {
-		
-		setPreferredSize(new Dimension(800,600));
+	private JButton menuPan_Btn_0;
+	private JButton menuPan_Btn_1;
+	private JButton menuPan_Btn_2;
+	private JButton menuPan_Btn_3;
+	private JButton menuPan_Btn_4;
+	private JButton menuPan_Btn_5;
+	
+	
+	
+	public SeatPanel() throws SQLException {
+
+		setPreferredSize(new Dimension(800, 600));
 		setBackground(Color.BLUE);
 		setLayout(null);
-		//-----------------------
-		//좌석 선택시 화면
+		// ------------------------------
+		DB = new DbDAO();
+		data = new DataInfo();
+	
 		
-		seatSelect_Panel=new JPanel();
-		seatSelect_Panel.setBounds(0,0,800,600);
+		cickArray = data.GetMenu("1");	
+		drinkArray = data.GetMenu("2");
+		sideArray = data.GetMenu("3");
+		
+		// -----------------------
+		// 좌석 선택시 화면
+
+		seatSelect_Panel = new JPanel();
+		seatSelect_Panel.setBounds(0, 0, 800, 600);
 		seatSelect_Panel.setLayout(null);
-		seatSelect_Panel.setVisible(false); //초기화시 안보이게 설정
-				
+		seatSelect_Panel.setVisible(false); // 초기화시 안보이게 설정
+
 		////
-		////선택한 좌석번호 표시 라벨
+		//// 선택한 좌석번호 표시 라벨
 		////
 		seatNum_Label = new JLabel("\uC88C\uC11D\uBC88\uD638(\uC608\uC815)");
 		seatNum_Label.setFont(new Font("나눔고딕", Font.BOLD, 24));
 		seatNum_Label.setBounds(12, 10, 156, 29);
 		seatSelect_Panel.add(seatNum_Label);
 		////
-		////주문내역 패널
+		//// 주문내역 패널
 		////
 		Order_Panel = new JPanel();
 		Order_Panel.setBackground(new Color(240, 255, 240));
-		Order_Panel.setBounds(12, 49, 375, 488);
+		Order_Panel.setBounds(12, 49, 330, 488);
+		Order_Panel.setLayout(null);
 		seatSelect_Panel.add(Order_Panel);
 		////
-		////메뉴선택 패널
+		//// 메뉴선택 패널
 		////
 		MenuPanel = new JPanel();
 		MenuPanel.setBackground(new Color(245, 255, 250));
-		MenuPanel.setBounds(418, 49, 353, 488);
+		MenuPanel.setBounds(371, 50, 400, 485);
+		MenuPanel.setLayout(null);
 		seatSelect_Panel.add(MenuPanel);
+		//------------------------------------------------------
+		//------------------------------------------------------
 		////
-		////선택화면 취소버튼
+		////
+		////메뉴판 패널
+		menuPan_Panel = new JPanel();
+		menuPan_Panel.setBackground(Color.PINK);
+		menuPan_Panel.setBounds(25, 114, 346, 337);
+		menuPan_Panel.setLayout(null);
+		MenuPanel.add(menuPan_Panel);
+		//////
+		//////
+		//////메뉴판 패널안 버튼
+		menuPan_Btn_0 = new JButton(cickArray.get(0).getName());
+		menuPan_Btn_0.setBounds(35, 69, 127, 59);
+		menuPan_Btn_0.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println(menuPan_Btn_0.getToolTipText());
+			}
+		});
+		menuPan_Panel.add(menuPan_Btn_0);
+		
+		menuPan_Btn_1 = new JButton(cickArray.get(1).getName());
+		menuPan_Btn_1.setBounds(188, 69, 127, 59);
+		menuPan_Btn_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println(menuPan_Btn_1.getToolTipText());
+			}
+		});
+		menuPan_Panel.add(menuPan_Btn_1);
+		
+		menuPan_Btn_2 = new JButton(cickArray.get(2).getName());
+		menuPan_Btn_2.setBounds(35, 188, 127, 59);
+		menuPan_Btn_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println(menuPan_Btn_2.getToolTipText());
+			}
+		});
+		menuPan_Panel.add(menuPan_Btn_2);
+		
+		menuPan_Btn_3 = new JButton(cickArray.get(3).getName());
+		menuPan_Btn_3.setBounds(188, 188, 127, 59);
+		menuPan_Btn_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println(menuPan_Btn_3.getToolTipText());
+			}
+		});
+		menuPan_Panel.add(menuPan_Btn_3);
+		
+		menuPan_Btn_4 = new JButton(drinkArray.get(4).getName());
+		menuPan_Btn_4.setBounds(35, 245, 127, 59);
+		menuPan_Btn_4.setVisible(false);
+		menuPan_Btn_4.setToolTipText(""+drinkArray.get(4).getPrice_menu());
+		menuPan_Btn_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println(menuPan_Btn_4.getToolTipText());
+			}
+		});
+		menuPan_Panel.add(menuPan_Btn_4);
+		
+		menuPan_Btn_5 = new JButton(drinkArray.get(5).getName());
+		menuPan_Btn_5.setToolTipText("");
+		menuPan_Btn_5.setBounds(188, 245, 127, 59);
+		menuPan_Btn_5.setVisible(false);
+		menuPan_Btn_5.setToolTipText(""+drinkArray.get(5).getPrice_menu());
+		menuPan_Btn_5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println(menuPan_Btn_5.getToolTipText());
+			}
+		});
+		menuPan_Panel.add(menuPan_Btn_5);
+
+		//------------------------------------------------------
+		//------------------------------------------------------
+		//////
+		//////
+		//////메뉴종류 라벨 : 해당 라벨 클릭시 각 종류별로 전환하는 이벤트가 Change_ () 실행
+		chic_Label = new JLabel("Chicken");
+		chic_Label.setFont(new Font("나눔고딕", Font.BOLD, 16));
+		chic_Label.setHorizontalAlignment(SwingConstants.CENTER);
+		chic_Label.setOpaque(true);
+		chic_Label.setBackground(Color.PINK);
+		chic_Label.setBounds(25, 59, 107, 55);
+		chic_Label.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Change_chic();
+			}
+		});
+
+		MenuPanel.add(chic_Label);
+
+		drink_Label = new JLabel("Drink");
+		drink_Label.setFont(new Font("나눔고딕", Font.BOLD, 16));
+		drink_Label.setHorizontalAlignment(SwingConstants.CENTER);
+		drink_Label.setOpaque(true);
+		drink_Label.setBackground(Color.ORANGE);
+		drink_Label.setBounds(144, 59, 107, 55);
+		drink_Label.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Change_drink();
+			}
+		});
+		MenuPanel.add(drink_Label);
+
+		side_Label = new JLabel("Side");
+		side_Label.setFont(new Font("나눔고딕", Font.BOLD, 16));
+		side_Label.setHorizontalAlignment(SwingConstants.CENTER);
+		side_Label.setOpaque(true);
+		side_Label.setBackground(Color.YELLOW);
+		side_Label.setBounds(261, 59, 110, 55);
+		side_Label.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Change_side();
+			}
+		});
+		MenuPanel.add(side_Label);
+		////
+		//// 선택화면 취소버튼
 		////
 		select_Cancel_Button = new JButton("\uCDE8\uC18C");
 		select_Cancel_Button.setFont(new Font("나눔고딕", Font.BOLD, 24));
 		select_Cancel_Button.setBounds(418, 547, 100, 40);
 		seatSelect_Panel.add(select_Cancel_Button);
 		////
-		////선택화면 확인버튼
+		//// 선택화면 확인버튼
 		////
 		select_Confirm_Button = new JButton("\uD655\uC778");
 		select_Confirm_Button.setFont(new Font("나눔고딕", Font.BOLD, 24));
 		select_Confirm_Button.setBounds(544, 547, 100, 40);
 		seatSelect_Panel.add(select_Confirm_Button);
 		////
-		////선택화면 결제버튼
+		//// 선택화면 결제버튼
 		////
 		select_Pay_Button = new JButton("\uACB0\uC81C");
 		select_Pay_Button.setFont(new Font("나눔고딕", Font.BOLD, 24));
 		select_Pay_Button.setBounds(671, 547, 100, 40);
 		seatSelect_Panel.add(select_Pay_Button);
-				
+
 		add(seatSelect_Panel);
+
+	}
+	//버튼 4개-> 6개 전환 [Change->에서 사용]
+	void Btn4To6() {
+		menuPan_Btn_0.setBounds(35, 36, 127, 59);
+		menuPan_Btn_1.setBounds(188, 36, 127, 59);
+		menuPan_Btn_2.setBounds(35, 145, 127, 59);
+		menuPan_Btn_3.setBounds(188, 145, 127, 59);
 		
+		menuPan_Btn_4.setVisible(true);
+		menuPan_Btn_5.setVisible(true);
+	
+	}
+	//버튼 6개-> 4개 전환
+	void Btn6To4() {
+		menuPan_Btn_0.setBounds(35, 69, 127, 59);
+		menuPan_Btn_1.setBounds(188, 69, 127, 59);
+		menuPan_Btn_2.setBounds(35, 188, 127, 59);
+		menuPan_Btn_3.setBounds(188, 188, 127, 59);
+		
+		menuPan_Btn_4.setVisible(false);
+		menuPan_Btn_5.setVisible(false);
+	
+	}
+	
+	public void Change_chic() {
+	
+		Btn6To4();
+		menuPan_Panel.setBackground(Color.PINK);
+		menuPan_Btn_0.setText(cickArray.get(0).getName());
+		menuPan_Btn_0.setToolTipText(""+cickArray.get(0).getPrice_menu());
+		menuPan_Btn_1.setText(cickArray.get(1).getName());
+		menuPan_Btn_1.setToolTipText(""+cickArray.get(1).getPrice_menu());
+		menuPan_Btn_2.setText(cickArray.get(2).getName());
+		menuPan_Btn_2.setToolTipText(""+cickArray.get(2).getPrice_menu());
+		menuPan_Btn_3.setText(cickArray.get(3).getName());
+		menuPan_Btn_3.setToolTipText(""+cickArray.get(3).getPrice_menu());
+	}
+
+	public void Change_drink() {
+	
+		Btn4To6();
+		menuPan_Panel.setBackground(Color.ORANGE);
+		menuPan_Btn_0.setText(drinkArray.get(0).getName());
+		menuPan_Btn_0.setToolTipText(""+drinkArray.get(0).getPrice_menu());
+		menuPan_Btn_1.setText(drinkArray.get(1).getName());
+		menuPan_Btn_1.setToolTipText(""+drinkArray.get(1).getPrice_menu());
+		menuPan_Btn_2 .setText(drinkArray.get(2).getName());
+		menuPan_Btn_2.setToolTipText(""+drinkArray.get(2).getPrice_menu());
+		menuPan_Btn_3 .setText(drinkArray.get(3).getName());
+		menuPan_Btn_3.setToolTipText(""+drinkArray.get(3).getPrice_menu());
+	}
+	public void Change_side() {
+	
+		Btn6To4();
+		menuPan_Panel.setBackground(Color.YELLOW);
+		menuPan_Btn_0.setText(sideArray.get(0).getName());
+		menuPan_Btn_0.setToolTipText(""+sideArray.get(0).getPrice_menu());
+		menuPan_Btn_1.setText(sideArray.get(1).getName());
+		menuPan_Btn_1.setToolTipText(""+sideArray.get(1).getPrice_menu());
+		menuPan_Btn_2.setText(sideArray.get(2).getName());
+		menuPan_Btn_2.setToolTipText(""+sideArray.get(2).getPrice_menu());
+		menuPan_Btn_3.setText(sideArray.get(3).getName());
+		menuPan_Btn_3.setToolTipText(""+sideArray.get(3).getPrice_menu());
 	}
 }
